8a25130e59775fcd7cf672c20852d132e11edc55
makshoop <makshoope@gmail.com>
2022-08-25 22:50:33 +0300

    1.Variables

---
+++ app.js
@@ -0,0 +1,26 @@
+//1 Переменные
+// var name = 'Maks'
+// устаревший способ. в дальнейшем можно
+// изменять значение переменной
+// Пример: 
+
+// name
+
+// console.log(name)
+//camelCase
+const firstName = 'Maks'
+const lastname = 'Kashcheev' //string
+// предпочтительно использование тега const 
+
+let age = 23 //number
+// так же можно изменять переменную аналогично с тегом var
+
+const isProgrammer = true //boolean
+
+const _ = 'private'
+const $ = 'some value'
+
+// const if = 'ffff' ошибочная переменная т.к. использовано зарезервиванное слово
+
+const withNum5 = '5'
+// const 5withNum = '5' так же ошиьочно, из-за цифры в начале
